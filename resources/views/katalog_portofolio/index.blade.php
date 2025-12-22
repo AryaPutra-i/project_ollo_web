9dee57613e21092df6439270e5f2a27ce5fe404a
AryaPutra-i <roger23dt@gmail.com>
2025-12-22 09:38:19 +0700

    update view katalog protofolio

--- resources/views/katalog_portofolio/index.blade.php
+++ resources/views/katalog_portofolio/index.blade.php
@@ -65,7 +65,7 @@
             display: flex;
             flex-direction: column;
             font-family: "poppins", sans-serif;
-            color: black;
+            color: #221153;
 
         }
 
@@ -92,16 +92,61 @@
             border-radius: 10px;
         }
 
-        .swiper{
+        .swiper {
             width: 100%;
         }
 
-        .myswiper{
+        .myswiper {
             overflow: hidden;
             /* max-width: 1200px; */
         }
 
-       
+        .link_show_more{
+            
+            font-family: "poppins", sans-serif;
+            font-weight: bold;
+            text-align: center;
+            justify-items: center;
+            align-items: center;
+            color: #221153;
+        }
+
+        .image_kata-kata{
+            background-image: url({{ asset('images/glenn-carstens-peters-npxXWgQ33ZQ-unsplash.jpg') }});
+            width: 100%;
+            height: 80vh;
+            background-size: cover;
+            background-repeat: no-repeat;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            mask-image: linear-gradient(to top, #7c4dff 60%, transparent);
+            
+        }
+
+        .kata-kata{
+            font-family: "poppins", sans-serif;
+            font-weight: bold;
+            color: white;
+            font-size: 50px;
+        }
+
+        .deskrips_kata-kata{
+            font-family: "poppins", sans-serif;
+            justify-items: center;
+        }
+        
+        .subtext_kata-kata{
+            font-family: "poppins", sans-serif;
+            justify-items: center;
+            color: white;
+            font-size: 20px;
+        }
+
+        
+
+
+
     </style>
     <div class="slide_porto d-xxl-flex px-5 px-md-5 mt-5">
         <h1 class="title_my_design">My Design</h1>
@@ -115,12 +160,28 @@
             <img src="{{ asset('images/arya_api_super brief 2.jpg') }}" alt="design3" class="design_aku mx-2 swiper-slide">
             <img src="{{ asset('images/challenge design 4 rev.jpg') }}" alt="design4" class="design_aku mx-2 swiper-slide">
             <img src="{{ asset('images/Challenge design 3.jpg') }}" alt="design5" class="design_aku mx-2 swiper-slide">
-            <img src="{{ asset('images/Challenge design 2 revisi.jpg') }}" alt="design6" class="design_aku mx-2 swiper-slide">
+            <img src="{{ asset('images/Challenge design 2 revisi.jpg') }}" alt="design6"
+                class="design_aku mx-2 swiper-slide">
         </div>
         <div class="swiper-pagination"></div>
         <div class="swiper-button-prev"></div>
         <div class="swiper-button-next"></div>
     </div>
+
+    <div class="link_show_more mt-4 ">
+        <a class="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover text-black"
+        href="#">
+        Show More Portofolio
+    </a>
+    </div>
+
+    <div class="image_kata-kata px-5 px-md-5 mt-5">
+        <div class="deskripsi_kata-kata">
+        <h1 class="kata-kata">"Karya yang bagus adalah karya yang kamu sukai"</h1>
+        <h2 class="subtext_kata-kata"> Saya membantu bisnis dan brand membangun visual yang profesional dan konsisten melalui fotografi produk, makanan, dan campaign digital.</h2>
+        </div>
+    </div>
+    
 @endsection
 
 @push('scripts')
@@ -163,6 +224,3 @@
         });
     </script>
 @endpush
-
-
-
