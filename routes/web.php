21cb22e5b013e4ad64884d620907aae36052f41a
AryaPutra-i <roger23dt@gmail.com>
2025-12-24 15:48:28 +0700

    edit dashboard

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,7 @@ Route::get('/', function () {
 Route::get('/welcome', function () {
     return view('Login_register.welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard_frelancer.index');
+});
